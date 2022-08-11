fc2c1f382ee710f26a0524756bca5da92c69059b
Gavgastyolo <97683400+Gavgastyolo@users.noreply.github.com>
2022-08-11 17:07:33 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use( express.json() )
+
+app.listen(
+    PORT,
+    () => console.log(`its alive on http://localhost:${PORT}`)
+)
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt:'Shirt',
+        color:'white'
+
+    })
+});
+
+app.get('/shoe', (req, res)=> {
+    res.status(200).send({
+        Shoe:'nike',
+        color:'black'
+    })
+})
+
+app.post('/tshirt/:id', (req, res)=>{
+
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if(!logo) {
+        res.status(418).send({ message: 'We need a logo'})
+    }
+
+    res.send({
+        tshirt:`tshirt with your ${logo} and ID of ${id}`,
+    })
+
+
+})
+
